d8d415c76bad8f95d4e63f68f24aa0360727cdb9
Tarcisio Andrade <tarcisioandrade016@gmail.com>
2023-02-28 09:41:03 -0300

    server.ts configured

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,22 @@
+import path from "path";
+
+import express from "express";
+import mustache from "mustache-express";
+
 require("dotenv").config();
+
+const app = express();
+
+app.set("view engine", "mustache");
+app.set("views", path.join(__dirname, "views"));
+app.engine("mustache", mustache());
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.use(express.json());
+
+// Routes
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server is running on port ${process.env.PORT}`),
+);
