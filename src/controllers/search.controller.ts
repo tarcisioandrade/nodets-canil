1265574c50950292fd3fbc182d17ffa0520261b7
Tarcisio Andrade <tarcisioandrade016@gmail.com>
2023-03-01 10:21:49 -0300

    final project

--- src/controllers/search.controller.ts
+++ src/controllers/search.controller.ts
@@ -1,5 +1,18 @@
+import Pet from "@models/Pet";
 import { Request, Response } from "express";
 
 export const search = (req: Request, res: Response) => {
-  res.render("pages/home");
+  const search = req.query.q;
+
+  if (!search) {
+    res.redirect("/");
+    return;
+  }
+
+  const list = Pet.getFromName(search as string);
+
+  res.render("pages/page", {
+    list,
+    search,
+  });
 };
